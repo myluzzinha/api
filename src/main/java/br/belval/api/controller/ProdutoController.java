7b60e689ff966d51f890ed3e10bb53a709456da4
ProfNpc <professornpc@gmail.com>
2026-04-29 20:18:24 -0300

    Foi adicionado o método obterProdutos() que chama findAll()

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -18,6 +18,11 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
 	
+	public ResponseEntity<Iterable<Produto>> obterProdutos() {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(repository.findAll());
+	}
 	
 	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	@PostMapping("/produtos")
