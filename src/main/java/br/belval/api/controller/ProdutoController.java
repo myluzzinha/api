2d89127454f40a36e052ece1ade480394bbbd3e9
Emilly <milu.silvaj@gmail.com>
2026-04-29 22:50:39 -0300

    ajustes no controle

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
 	
+	/**
+	 * Retorna todos os produtos
+	 * @return
+	 */
+	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
 		return ResponseEntity
 			.status(HttpStatus.OK)
