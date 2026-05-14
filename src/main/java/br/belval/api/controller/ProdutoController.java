bb100ea7f03e53c68406dde9796a31aed37ac6c1
Emilly <milu.silvaj@gmail.com>
2026-05-13 22:09:10 -0300

    13/05/2026

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -1,12 +1,16 @@
 package br.belval.api.controller;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,34 +19,73 @@ import br.belval.api.repository.ProdutoRepository;
 
 @RestController
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoRepository repository;
-	
+
 	/**
 	 * Retorna todos os produtos
+	 * 
 	 * @return
 	 */
 	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
-		return ResponseEntity
-			.status(HttpStatus.OK)
-			.body(repository.findAll());
-	}
-	
-	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
-	@PostMapping("/produtos")
-	public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
-		
-		produto.setDataCriacao(LocalDateTime.now());
-		
-		System.out.println("Chegou : " + produto.toString());
-		
-		repository.save(produto);
-		
-		return ResponseEntity
-				.status(HttpStatus.CREATED)
-				.body(produto);
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
 	}
 
+	// curl POST http://localhost:8080/produtos -H "Content-Type: application/json;
+	// Charset=utf-8" -d @produto-pao.json
+	@PostMapping("/produtos")
+	public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
+
+		produto.setDataCriacao(LocalDateTime.now());
+
+		System.out.println("Chegou : " + produto.toString());
+
+		repository.save(produto);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+	}
+
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Object> buscarPorId(@PathVariable(value = "id") Integer id) {
+
+		Optional<Produto> produtoOpt = repository.findById(id);
+
+		if (produtoOpt.isPresent()) {
+			return ResponseEntity.status(HttpStatus.OK).body(produtoOpt.get());
+		}
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não econtrado!");
+	}
+
+	@PutMapping("/produtos/{id}")
+	public ResponseEntity<Object> atualizarProduto(@PathVariable Integer id, @RequestBody Produto produto) {
+
+		Optional<Produto> produtoOpt = repository.findById(id);
+
+		if (produtoOpt.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
+		}
+
+		produto.setId(id);
+		produto.setDataCriacao(produtoOpt.get().getDataCriacao());
+
+		repository.save(produto);
+
+		return ResponseEntity.status(HttpStatus.OK).body("Produto atualizado com sucesso!");
+	}
+
+	@DeleteMapping("/produtos/{id}")
+	public ResponseEntity<String> apagarProduto(@PathVariable Integer id) {
+
+		Optional<Produto> produtoOpt = repository.findById(id);
+
+		if (produtoOpt.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
+		}
+		repository.deleteById(id);
+
+		return ResponseEntity.status(HttpStatus.OK).body("Prosuto apagado com sucesso!");
+	}
 }
